d63e9aa0a1b01d857d6b1b27be392d43fe7701fe
Sadgo <sadgobela@gmail.com>
2019-08-06 15:18:12 +0400

    Finished points counter script

--- js/main.js
+++ js/main.js
@@ -5,37 +5,27 @@
     };
 
     Countdown.prototype.init = function () {
-        this.endOfMonth = new Date("Aug 31, 2019 00:00:00").getTime()
+        this.endOfMonth = new Date("Aug 31, 2019 00:00:00").getTime();
         this.intervalId = setInterval(() => {
             this.timer();
         }, 1000);
-    }
+    };
     Countdown.prototype.timer = function () {
-        // Set the date we're counting down to
 
-// Update the count down every 1 second
-
-            // Get today's date and time
             const now = new Date().getTime();
 
-            // Find the distance between now and the count down date
             const distance = this.endOfMonth - now;
 
-            // Time calculations for days, hours, minutes and seconds
             const days = Math.floor(distance / (1000 * 60 * 60 * 24));
             const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 
-            // Display the result in the element with id="demo"
             document.getElementById('day').innerHTML = days;
             document.getElementById('hour').innerHTML = hours;
             document.getElementById('minute').innerHTML = minutes;
 
-
-            // If the count down is finished, write some text
             if (distance < 0) {
                 clearInterval(this.intervalId);
-                // document.getElementById("demo").innerHTML = "EXPIRED";
             }
     };
     (new Countdown()).init();
